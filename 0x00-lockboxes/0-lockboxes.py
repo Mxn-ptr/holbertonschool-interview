24019e2560f48a7c3e8d255f68eb0278406ad730
Mxn-ptr <maxence0@hotmail.fr>
2022-12-05 12:22:23 +0100

    Add task

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a function to see if all the boxes can be opened"""
+
+def canUnlockAll(boxes):
+    """function that checks if all the boxes can be opened"""
+    opened = [0]
+    new_key = True
+    while new_key:
+        new_key = False
+        for box in boxes:
+            if boxes.index(box) in opened:
+                for i in box:
+                    if i not in opened:
+                        new_key = True
+                        opened.append(i)
+
+
+    if len(boxes) == len(opened):
+        return True
+    else:
+        return False
