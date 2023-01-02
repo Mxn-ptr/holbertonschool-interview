c7c6689d2dc3a2cf85601e205a2135b1d9265fbf
Mxn-ptr <maxence0@hotmail.fr>
2023-01-02 21:12:53 +0100

    Add the project

---
+++ 0X03-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module for the task 0 """
+
+
+def minOperations(n):
+    """ method that calculates the fewest number of operations to result in exactly n H characters in the file """
+    i = 1
+    string = "H"
+    while (len(string) < n) :
+        if ((n % len(string)) == 0):
+            string_cpy = string
+            i += 1
+        string = string + string_cpy
+        i += 1
+
+    return i - 1
+
