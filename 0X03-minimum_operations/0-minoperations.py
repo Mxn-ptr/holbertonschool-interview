549689bf54bf9c84083aa4d53a81dd488d8b6954
Mxn-ptr <maxence0@hotmail.fr>
2023-01-03 14:15:27 +0100

    Change an operator

--- 0X03-minimum_operations/0-minoperations.py
+++ 0X03-minimum_operations/0-minoperations.py
@@ -6,12 +6,11 @@ def minOperations(n):
     """ method that calculates the fewest number of operations to result in exactly n H characters in the file """
     i = 1
     string = "H"
-    while (len(string) < n) :
+    while (len(string) != n) :
         if ((n % len(string)) == 0):
             string_cpy = string
             i += 1
         string = string + string_cpy
         i += 1
-
     return i - 1
 
