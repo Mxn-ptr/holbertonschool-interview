4d3bdfe82415914eb2870cc21668a5d84188f597
Mxn-ptr <maxence0@hotmail.fr>
2023-01-04 12:40:38 +0100

    Fix task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,16 +3,16 @@
 
 
 def minOperations(n):
-    """ method that calculates the fewest number of operations to result in exactly n H characters in the file """
+    """ method that calculates the fewest number of operations
+    to result in exactly n H characters in the file """
     if (n <= 0 or not isinstance(n, int)):
         return 0
     i = 1
     string = "H"
-    while (len(string) != n) :
+    while (len(string) != n):
         if ((n % len(string)) == 0):
             string_cpy = string
             i += 1
         string = string + string_cpy
         i += 1
     return i - 1
-
