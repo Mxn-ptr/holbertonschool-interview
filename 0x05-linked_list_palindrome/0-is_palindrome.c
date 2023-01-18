3ed9484adf7a6eddc5103ad7ec3983be23f9302d
Mxn-ptr <maxence0@hotmail.fr>
2023-01-18 13:34:25 +0100

    Fix task 0

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,47 +1,27 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h : pointer to the head of the list
- * Return: number of elements
- */
-
-size_t listint_len(const listint_t *h)
-{
-	size_t nodes = 0;
-
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
-}
-
-/**
  * check_palindrome - check recursively if a list is a palindrome
  * @node: node to check
  * @k: index to the node to check with node
  * Return: 1 if same, 0 if not
 */
-int check_palindrome(listint_t *node, int k)
+int check_palindrome(listint_t **node, listint_t *next)
 {
-	listint_t *check;
-	int i;
 	int is_palindrome = 1;
 
-	check = node;
-	if (is_palindrome == 1 && k > 0)
-	{
-		for (i = 0; i < k; i++)
-			check = check->next;
-		if (node->n == check->n)
-		{
-			is_palindrome = check_palindrome(node->next, (k - 2));
-		}
-		else
-			is_palindrome = 0;
-	}
+	if (next == NULL)
+		return (1);
+
+	is_palindrome = check_palindrome(node, next->next);
+	
+	if (is_palindrome == 0)
+		return (0);
+
+	is_palindrome = ((*node)->n == next->n);
+
+	(*node) = (*node)->next;
+
 	return (is_palindrome);
 }
 
@@ -52,11 +32,7 @@ int check_palindrome(listint_t *node, int k)
 */
 int is_palindrome(listint_t **head)
 {
-	size_t len = 0;
-
 	if (*head == NULL || head == NULL)
 		return (1);
-
-	len = listint_len(*head);
-	return (check_palindrome(*head, (len - 1)));
+	return (check_palindrome(head, *head));
 }
