f11c8d932adab97dc673d5b1d02137eaf1ce0dd9
Mxn-ptr <maxence0@hotmail.fr>
2023-01-23 18:24:08 +0100

    Add project

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+
+
+count = 0
+total_size = 0
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def print_line():
+    print("File size: {}".format(total_size))
+    for code in sorted(codes.keys()):
+        if codes[code] > 0:
+            print("{}: {}".format(code, codes[code]))
+
+
+if __name__ == '__main__':
+    try:
+        for line in sys.stdin:
+            split_line = line.split()
+            size = int(split_line[-1])
+            status = int(split_line[-2])
+            count += 1
+            total_size += size
+            codes[status] += 1
+
+            if count % 10 == 0:
+                print_line()
+    except KeyboardInterrupt:
+        print_line()
