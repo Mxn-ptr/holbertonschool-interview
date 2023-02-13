fe1d35a4ed839550924b0791a9e63eaea400c69c
Mxn-ptr <maxence0@hotmail.fr>
2023-02-13 11:53:20 +0100

    Add task

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module for validate utf-8 """
+
+
+def validUTF8(data):
+    """ determines if a given data set represents a valid UTF-8 encoding """
+    try:
+        bytes(data)
+        return True
+    except ValueError:
+        return False
