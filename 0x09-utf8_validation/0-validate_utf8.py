0fac52710b249fe13292baa9ec9b8f2f9d8f3d83
Mxn-ptr <maxence0@hotmail.fr>
2023-02-15 14:17:34 +0100

    Fix task

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,9 @@
 
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
+    for d in data:
+        if not isinstance(d, int):
+            return False
     try:
         bytes(data)
         return True
