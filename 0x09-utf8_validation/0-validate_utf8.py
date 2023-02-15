cc289bf8709e1e483febb8fd5bdd94ca407425f3
Mxn-ptr <maxence0@hotmail.fr>
2023-02-15 14:27:01 +0100

    Fix task

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,11 +4,26 @@
 
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
-    for d in data:
-        if not isinstance(d, int):
-            return False
-    try:
-        bytes(data)
-        return True
-    except ValueError:
-        return False
+    number_bytes = 0
+
+    for number in data:
+        binary = format(number, '#010b')[-8:]
+
+        if number_bytes == 0:
+            for bit in binary:
+                if bit == '0': break
+                number_bytes += 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (binary[0] == '1' and binary[1] == '0'):
+                return False
+
+        number_bytes -= 1
+
+    return number_bytes == 0
