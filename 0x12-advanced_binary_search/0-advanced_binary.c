7dc3245e341d7f73fe0eb50ae8cbb5cd035f1a1b
Mxn-ptr <maxence0@hotmail.fr>
2023-05-03 00:39:44 +0200

    Add project

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,69 @@
+#include "search_algos.h"
+
+/**
+ * print_array - print the array
+ * @array: array
+ * @start: start to print
+ * @end: end of to print
+*/
+void print_array(int *array, int start, int end)
+{
+	int i;
+
+	printf("Searching in array: ");
+
+	for (i = start; i < end; i++)
+	{
+		if (i == end - 1)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
+}
+
+/**
+ * advanced_binary_recurs - search with recursion
+ * @array: array to search in
+ * @start: index to start
+ * @end: index to end
+ * @value: value to found
+ *
+ * Return: index of value or -1
+*/
+int advanced_binary_recurs(int *array, int start, int end, int value)
+{
+	int middle = (end - start) / 2 + start;
+
+	print_array(array, start, end);
+
+	if (start == end)
+		return (-1);
+	if (array[middle] == value && array[middle - 1] != value)
+	{
+		print_array(array, middle, end);
+		return (middle);
+	}
+
+	if (array[middle] <= value)
+	{
+		return (advanced_binary_recurs(array, middle, end, value));
+	}
+	else
+	{
+		return (advanced_binary_recurs(array, start, middle, value));
+	}
+	return (-1);
+}
+
+/**
+ * advanced_binary - search with recursion
+ * @array: array to search in
+ * @size: size of the array
+ * @value: value to found
+ *
+ * Return: index of value or -1
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (advanced_binary_recurs(array, 0, size, value));
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
