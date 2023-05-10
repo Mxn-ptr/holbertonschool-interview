bc0fc9adc53f68d48b6e35d0299855b94a6a69ab
Mxn-ptr <maxence0@hotmail.fr>
2023-05-10 15:57:33 +0200

    Fix

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -2,6 +2,7 @@
 """ Defines recursive function """
 import requests
 
+
 def count_words(subreddit, word_list, after=None, counts={}):
     """ queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords
@@ -9,26 +10,22 @@ def count_words(subreddit, word_list, after=None, counts={}):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'My_Agent'}
     params = {'after': after}
-    res = requests.get(url, headers=headers, allow_redirects=False, params=params)
+    res = requests.get(url, headers=headers, allow_redirects=False,
+                       params=params)
     if res.status_code != 200:
         return
-    
+
     data = res.json().get('data')
     posts = data.get('children')
     after = data.get('after')
 
-    for word in word_list:
-        word = word.lower()
-    
     for post in posts:
-        title = post['data']['title'].lower()
+        title = post['data']['title'].lower().split()
         for word in word_list:
-            if word in title:
-                if word in counts:
-                    counts[word] += 1
-                else:
-                    counts[word] = 1
-    
+            word = word.lower()
+            n = title.count(word)
+            counts[word] = counts.setdefault(word, 0) + n
+
     if after is not None:
         count_words(subreddit, word_list, after, counts)
     else:
