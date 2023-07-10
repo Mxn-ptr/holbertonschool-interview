1c67f2952691095c29370378a82044a5fbc0a3e6
Mxn-ptr <maxence0@hotmail.fr>
2023-07-10 10:03:09 +0200

    Add project

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,68 @@
+#include "list.h"
+
+/**
+ * add_node_end - Add a node at the end of a circular list linked
+ * @list: pointer to the head of the list
+ * @str: string to add
+ *
+ * Return: pointer to the new node or NULL if fail
+*/
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = str;
+
+	if (*list == NULL)
+	{
+		new->next = new;
+		new->prev = new;
+		(*list) = new;
+	}
+	else
+	{
+		new->next = (*list);
+		new->prev = (*list)->prev;
+		(*list)->prev->next = new;
+		(*list)->prev = new;
+	}
+	return (new);
+}
+
+/**
+ * add_node_begin - Add a node at the start of a circular list linked
+ * @list: pointer to the head of the list
+ * @str: string to add
+ *
+ * Return: pointer to the new node or NULL if fail
+*/
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = str;
+
+	if (*list == NULL)
+	{
+		new->next = new;
+		new->prev = new;
+		(*list) = new;
+	}
+	else
+	{
+		new->next = (*list);
+		new->prev = (*list)->prev;
+		(*list)->prev->next = new;
+		(*list)->prev = new;
+		(*list) = new;
+	}
+	return (new);
+}
