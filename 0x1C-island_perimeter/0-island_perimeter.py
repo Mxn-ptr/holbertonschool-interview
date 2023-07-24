ea0f6b7782aece0febe9f284b01e5e6d2b6e676b
Mxn-ptr <maxence0@hotmail.fr>
2023-07-24 15:27:30 +0200

    Fix task

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
 Island perimeter module
 """
 
+
 def island_perimeter(grid):
     """ Return the perimeter of the island:
         - 0 is water
@@ -11,13 +12,27 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
-            if grid[i][j] == 1 and grid[i + 1][j] == 0:
-                perimeter += 1
-            if grid[i][j] == 1 and grid[i - 1][j] == 0:
-                perimeter += 1
-            if grid[i][j] == 1 and grid[i][j + 1] == 0:
-                perimeter += 1
-            if grid[i][j] == 1 and grid[i][j - 1] == 0:
-                perimeter += 1
-    return perimeter
+            if grid[i][j] == 1:
+                if (i + 1 > len(grid) - 1):
+                    perimeter += 1
+                elif (grid[i + 1][j] == 0):
+                    perimeter += 1
             
+            if grid[i][j] == 1:
+                if (i - 1 < 0):
+                    perimeter += 1
+                elif (grid[i - 1][j] == 0):
+                    perimeter += 1
+            
+            if grid[i][j] == 1:
+                if (j + 1 > len(grid[i]) - 1):
+                    perimeter += 1
+                elif (grid[i][j + 1] == 0):
+                    perimeter += 1
+            
+            if grid[i][j] == 1:
+                if (j - 1< 0):
+                    perimeter += 1
+                elif grid[i][j - 1] == 0:
+                    perimeter += 1
+    return perimeter
